983c052e1d18430c14fc85db4bd576fbfbfd747d
davi <manelv8@gmail.com>
2016-05-17 16:57:51 -0300

    finished uni 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ActiveRecord::Base
 	validates_confirmation_of :password
 	validates_length_of :bio, minimum: 30, allow_blank: true
 	validate :email_format
+	validates_uniqueness_of :email
 
-	
+private
+
+	def email_format
+		errors.add(:email,:invalid) unless email.match(EMAIL_REGEXP)
+	end
+
+
 end
